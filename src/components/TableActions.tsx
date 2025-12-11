2739c86894797610046b2f097b7fae6e42994b68
Muhammad Bilal <bilal.kalri@gmail.com>
2025-12-11 19:22:41 +0500

    Edit seller and approver

--- src/components/TableActions.tsx
+++ src/components/TableActions.tsx
@@ -2,14 +2,14 @@ import Icon from './Icon'
 
 interface PropsTypes {
     onClick: (id:unknown,type:string) => void;
-    id: unknown
+    item: unknown
 }
 
-const TableActions = ({id,onClick}:PropsTypes) => {
+const TableActions = ({item,onClick}:PropsTypes) => {
   return (
     <div className='flex items-center gap-2'>
-        <button onClick={()=>onClick(id,'delete')}><Icon name='trash'/></button>
-        <button onClick={()=>onClick(id,'edit')}><Icon name='edit'/></button>
+        <button onClick={()=>onClick(item,'delete')}><Icon name='trash'/></button>
+        <button onClick={()=>onClick(item,'edit')}><Icon name='edit'/></button>
     </div>
   )
 }
